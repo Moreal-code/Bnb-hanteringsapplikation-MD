46b2f34742c618b7a34066108b3e1028bad27831
Moreal-code <mohamed.dukale@yh.nackademin.se>
2025-11-04 14:32:17 +0100

    implement reservation booking system
    - Add reservation component and booking functionality
    - Create trips and reservations screens with server components
    - Add reservation routes and data fetching logic

--- app/listings/[listingid]/page.tsx
+++ app/listings/[listingid]/page.tsx
@@ -2,6 +2,7 @@ import getCurrentUser from "@/app/actions/getCurrentUser";
 import getListingById from "@/app/actions/getListingById";
 import EmptyState from "@/app/components/EmptyState";
 import ListingClient from "./ListingClient";
+import getReservations from "@/app/actions/getReservations";
 
 interface IParams {
   listingid: string;
@@ -9,13 +10,20 @@ interface IParams {
 
 const ListingPage = async ({ params }: { params: IParams }) => {
   const listing = await getListingById({ listingId: params.listingid });
+  const reservations = await getReservations({ listingId: params.listingid });
   const currentUser = await getCurrentUser();
 
   if (!listing) {
     return <EmptyState />;
   }
 
-  return <ListingClient listing={listing} currentUser={currentUser} />;
+  return (
+    <ListingClient
+      listing={listing}
+      currentUser={currentUser}
+      reservations={reservations}
+    />
+  );
 };
 
 export default ListingPage;
--- app/types/index.ts
+++ app/types/index.ts
@@ -1,4 +1,18 @@
-import { User } from "@/app/generated/prisma";
+import { Listing, Reservation, User } from "@/app/generated/prisma";
+
+export type SafeListing = Omit<Listing, "createAt"> & {
+  createAt: string;
+};
+
+export type SafeReservation = Omit<
+  Reservation,
+  "createdAt" | "startDate" | "endDate" | "listing"
+> & {
+  createdAt: string;
+  startDate: string;
+  endDate: string;
+  listing: SafeListing;
+};
 
 export type SafeUser = Omit<
   User,
